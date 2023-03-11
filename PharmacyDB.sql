ca0485e573f2566d1b48a48c277bff02332bd2a4
Michael Babik <100808929+Iviichae7@users.noreply.github.com>
2023-03-11 13:06:43 +0000

    Adjustments
    Adjustments to the tables

--- PharmacyDB.sql
+++ PharmacyDB.sql
@@ -1,25 +1,20 @@
-CREATE TABLE IF NOT EXISTS `mydb`.`Customer` (
-  `Customer_ID` INT NULL,
-  `Name` VARCHAR(45) NULL,
-  `Address` VARCHAR(45) NULL,
-  `Phone_number` VARCHAR(45) NULL,
-  `Email` VARCHAR(45) NULL,
+CREATE TABLE Customer (
+  Customer_ID INT NOT NULL,
+  Name VARCHAR(45) NOT NULL,
+  Address VARCHAR(45) NOT NULL,
+  Phone_number VARCHAR(45)NOT NULL,
+  Email VARCHAR(45) NOT NULL,
   PRIMARY KEY (`Customer_ID`))
-ENGINE = InnoDB
+  );
 
-CREATE TABLE IF NOT EXISTS `mydb`.`Prescription` (
-  `Prescription_ID` INT NULL,
-  `Medication_name` VARCHAR(45) NULL,
-  `Issued_Doctor` VARCHAR(45) NULL,
-  `Isssued_Date` DATE NULL,
-  `Customer_ID` INT NULL,
-  `Medication_ID` VARCHAR(45) NULL,
-  `Customer_Customer_ID` INT NOT NULL,
-  PRIMARY KEY (`Prescription_ID`, `Customer_Customer_ID`),
-  INDEX `fk_Prescription_Customer_idx` (`Customer_Customer_ID` ASC) VISIBLE,
-  CONSTRAINT `fk_Prescription_Customer`
-    FOREIGN KEY (`Customer_Customer_ID`)
-    REFERENCES `mydb`.`Customer` (`Customer_ID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
+
+CREATE TABLE Prescription (
+  Prescription_ID INT NOT NULL,
+  Medication_name VARCHAR(45) NOT NULL,
+  Issued_Doctor VARCHAR(45) NOT NULL,
+  Isssued_Date DATE NOT NULL,
+  Customer_ID INT NOT NULL,
+  Medication_ID VARCHAR(45) NOT NULL,
+  FOREIGN KEY (Customer_ID) REFERENCES Customer(Customer_ID),
+  FOREIGN KEY (Medication_ID) REFERENCES Medication(Medication_ID)
+);
